3e0f9367af6f08b3d7d8f16e2ffde125037e7049
DmitrySab <92842735+DmitrySab@users.noreply.github.com>
2021-10-26 17:45:36 +0400

    26.10

--- app/src/main/java/com/example/my1/MainActivity.kt
+++ app/src/main/java/com/example/my1/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.my1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val helloTV: TextView = findViewById(R.id.TV_hello)
+        helloTV.text = ""
     }
 }
