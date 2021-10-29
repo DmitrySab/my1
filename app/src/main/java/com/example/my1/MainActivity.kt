8c7d2822de98b4f0082f3476d61975aaf1d69bc1
DmitrySab <92842735+DmitrySab@users.noreply.github.com>
2021-10-29 18:23:45 +0400

    26.10

--- app/src/main/java/com/example/my1/MainActivity.kt
+++ app/src/main/java/com/example/my1/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.example.my1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+
+private const val TAG = 'MyActivity'
+private const val VALUE = 'ключик1'
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var helloTextView: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val helloTV: TextView = findViewById(R.id.TV_hello)
-        helloTV.text = ""
+        /*helloTV.text = R.string.hello*/
+        val buttonChange: Button = findViewById(R.id.button_change)
+        buttonChange.setOnClickListener {
+
+        }
+
+        Log.d ( TAG ,  "onCr")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(VALUE)
     }
 }
